264bcef483362525bd0c238d58c10111b8e9fe46
Geert Huls <geert.huls82@gmail.com>
2017-01-08 21:53:28 +0100

    Basic main page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HowToComponent } from './how-to/how-to.component';
 import { ExamplesComponent } from './examples/examples.component';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ExamplesComponent } from './examples/examples.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]

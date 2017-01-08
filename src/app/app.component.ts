d61d4ca2267d3caff07cca9e774f4b699e8b95b2
Geert Huls <geert.huls82@gmail.com>
2017-01-08 22:30:48 +0100

    Input text focus animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,35 @@ import { Component, trigger, state, style, transition, animate, keyframes, Input
       })),
       transition('inactive => active', animate("500ms ease-in")),
       transition('active => inactive', animate("500ms ease-out"))
+    ]),
+    trigger("note", [
+      state("inactive", style({
+        opacity: "0"
+      })),
+      state("active", style({
+        opacity: "1"
+      })),
+      transition("inactive => active", [
+        animate(300, keyframes([
+          style({ opacity: 0, transform: "translateY(0)", offset: 0 }),
+          style({ opacity: 1, transform: "translateY(-15px)", offset: 0.6 }),
+          style({ opacity: 1, transform: "translateY(0)", offset: 1 }),
+        ]))
+      ]),
+      transition("active => inactive", [
+        animate(300, keyframes([
+          style({ opacity: 1, transform: "translateY(0)", offset: 0 }),
+          style({ opacity: 1, transform: "translateY(-15px)", offset: 0.7 }),
+          style({ opacity: 0, transform: "translateY(100%)", offset: 1 }),
+        ]))
+      ])
     ])
   ]
 })
 export class AppComponent {
-  title = 'app works!';
+  state: string = 'inactive';
+
+  toggleFocus() {
+    this.state = (this.state === "inactive" ? "active" : "inactive");
+  }
 }

3abab6940d0cde124269bec1327ae716f6b83980
Geert Huls <geert.huls82@gmail.com>
2017-01-08 22:22:22 +0100

    Focus panel animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate, keyframes, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger("focusPanel", [
+      state("inactive", style({
+        transform: "scale(1)"
+      })),
+      state("active", style({
+        transform: "scale(1.04)"
+      })),
+      transition('inactive => active', animate("500ms ease-in")),
+      transition('active => inactive', animate("500ms ease-out"))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
